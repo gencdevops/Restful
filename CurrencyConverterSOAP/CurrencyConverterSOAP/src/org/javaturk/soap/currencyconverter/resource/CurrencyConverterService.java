c0ae6ae493510717284ebd73da4d2d1520f8705a
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-09 13:49:06 +0300

    interface CurrencyConverterService was added

--- CurrencyConverterSOAP/CurrencyConverterSOAP/src/org/javaturk/soap/currencyconverter/resource/CurrencyConverterService.java
+++ CurrencyConverterSOAP/CurrencyConverterSOAP/src/org/javaturk/soap/currencyconverter/resource/CurrencyConverterService.java
@@ -6,3 +6,18 @@ import org.javaturk.soap.currencyconverter.domain.Conversion;
 import org.javaturk.soap.currencyconverter.domain.Conversions;
 import org.javaturk.soap.currencyconverter.domain.Currency;
 
+public interface CurrencyConverterService {
+
+	Conversions getAllConversions();
+
+	List<Currency> getAllCurrencies();
+
+	Conversion convertByObject(Conversion conversionParameter);
+
+	Conversion convertByParam(Currency sourceCurrency, Currency targetCurrency, double amount);
+
+//	Conversion convertString(String sourceCurrencyString, String targetCurrencyString, double amount);
+//
+//	Conversion convertStringXML(String sourceCurrencyString, String targetCurrencyString, double amount);
+
+}
